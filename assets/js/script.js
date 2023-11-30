965642b0fe6b8f9afa181f5df0cf418ef023bfc2
Loris Schlatter <lorisschlatter@gmail.com>
2023-11-30 15:30:16 +0100

    ajout d'un codage pour bouton avis et commentaires

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 // AJOUT D'UN CAROUSEL SUR LA SECTION CARTE
 
+// Ici, on récupère les variables nécessaires (deux boutons, images et conteneur images)
+// La logique est d'incruster le code dans une fonction qui se déclenchera au click
+// sur le bouton gauche ou droit. Au click, la source de l'image prend +1 ou -1
+// Par exemple, on passe de l'image food_6_dark.jpg à food_7_dark.jpg
+// On ajoute ensuite une animation qui passe d'une opacité 0.7 à 1 pour faire un fondu
+// entre les images, le fond étant noir, on a donc un effet de fondu au noir léger et rapide.
+
 let image = document.querySelector(".carIMG img");
 let button = document.querySelector(".button");
 let button2 = document.querySelector(".button2");
@@ -41,7 +48,27 @@ button2.addEventListener("click", () => {
 
 })
 
+// BOUTON LAISSER UN AVIS
+
+// Si l'utilisateur ne rentre rien et appuie sur "OK" le message sera marqué comme annulé
+
+let avis = document.querySelector('.h4Center')
+
+avis.addEventListener("click" , function(){
+    message = window.prompt("Entrez votre avis !");
+    if (message.length){
+        alert("Avis envoyé. Merci !")
+    }else {
+        alert("Message annulé.")
+    }
+})
+
 // VERROUILLAGE BOUTON RESERVER
+
+// Chaque champ html possède une classe html "required". Lorsque que le champ est respecté,
+// la classe required disparait. Lorsque toutes les classes required sont enlevées,
+// le bouton submit est "débloqué" et devient clickable.
+// Une alert nous informe du bon déroulé de nos saisies et confirme notre réservation.
 
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("reservationForm");
@@ -87,6 +114,13 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // MENU BURGER
+
+// En responsive MOBILE ET TABLETTE, on cache la barre de navigation au profit
+// d'un logo type burger menu. Au click sur ce logo, la barre de nav apparait mais
+// cette fois-ci en display flex + flex direction column
+// Le code jongle donc entre html, css et javascript dans le sens où l'on fait ajouter
+// Une classe (html) avec un style précis (css) lors du click sur le bouton. Cette classe 
+// disparait lors d'un nouveau click (codé avec un toggle en javascript).
 
 var menu = document.getElementById("menu");
 var burger = document.getElementById("iconBurger");
